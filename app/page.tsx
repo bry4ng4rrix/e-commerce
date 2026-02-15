274e9ff68f28ac8df8fcb5f604cd378f82946bf9
bry4ng4rrix <bryanmfb4@gmail.com>
2026-02-15 11:26:44 +0300

    remove

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,15 @@
+import { Card } from "@/components/ui/card"
+import { Button } from "@/components/ui/button"
 
 const page = () => {
   return (
-    <div>
-      
+    <div className="bg-zinc-800 min-h-screen w-full  p-3  text-white">
+      <Card className="block p-5">
+        <Button variant='outline'>mety</Button>
+         
+        
+        
+         </Card>
     </div>
   )
 }
