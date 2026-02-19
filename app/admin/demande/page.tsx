7550a9555565b8285506c63ddb0763291b86aad6
bry4ng4rrix <bryanmfb4@gmail.com>
2026-02-19 18:12:48 +0300

    news

--- app/admin/demande/page.tsx
+++ app/admin/demande/page.tsx
@@ -1,63 +1,67 @@
-import { Card ,CardHeader ,CardContent ,CardFooter } from "@/components/ui/card"
+import { Card, CardHeader, CardContent, CardFooter } from "@/components/ui/card"
 import { CalendarDays, Check, ClipboardClock, Hourglass, X } from "lucide-react"
 import { Button } from "@/components/ui/button"
-import {Badge } from '@/components/ui/badge'
+import { Badge } from '@/components/ui/badge'
+
+
+
+
 const statItems = [
-  { titre : "Bulletins validés" , nombre : 285  , pourcent : "10%"} ,
-  { titre : "Inscrit" , nombre : 1024  , pourcent : "10%"} ,
-  { titre : "Document en attente" , nombre : 45  , pourcent : "10%"} ,
-  { titre : "demande d'Inscriptions" , nombre : 120  , pourcent : "10%"} ,
+  { titre: "Bulletins validés", nombre: 285, pourcent: "10%" },
+  { titre: "Inscrit", nombre: 1024, pourcent: "10%" },
+  { titre: "Document en attente", nombre: 45, pourcent: "10%" },
+  { titre: "demande d'Inscriptions", nombre: 120, pourcent: "10%" },
 ]
 
 const demandeItem = [
-  { titre : "demande de Bulletin " , date : '10/02/2026' ,status : "En attent" , nom : "Jean"} ,
-  { titre : "demande d'inscriptions " , date : '16/02/2026' ,status : "En attent"  , nom : "Bryan"} ,
-  { titre : "demande de Certificat " , date : '16/02/2026' ,status : "En attent"  , nom : "Bryan"} ,
+  { titre: "demande de Bulletin ", date: '10/02/2026', status: "En attent", nom: "Jean" },
+  { titre: "demande d'inscriptions ", date: '16/02/2026', status: "En attent", nom: "Bryan" },
+  { titre: "demande de Certificat ", date: '16/02/2026', status: "En attent", nom: "Bryan" },
 ]
 
 const billetItem = [
-  {nom : "Bryan" ,class : "2nd", date : '14/05/26' , motif : "maladie"},
-  {nom : "Jean" ,class : '2nd', date : '14/05/26' , motif : "maladie"},
-  {nom : "Paule" ,class :"6 eme", date : '14/05/26' , motif : "maladie"},
-  {nom : "Jerie" , date : '14/05/26' , motif : "maladie"},
-  {nom : "Eric" , date : '14/05/26' , motif : "maladie"},
+  { nom: "Bryan", class: "2nd", date: '14/05/26', motif: "maladie" },
+  { nom: "Jean", class: '2nd', date: '14/05/26', motif: "maladie" },
+  { nom: "Paule", class: "6 eme", date: '14/05/26', motif: "maladie" },
+  { nom: "Jerie", date: '14/05/26', motif: "maladie" },
+  { nom: "Eric", date: '14/05/26', motif: "maladie" },
 ]
 
 const reunionItem = [
-  {nom : "Bryan" , date : '14/05/26' , motif : "maladie"},
-  {nom : "Jean" , date : '14/05/26' , motif : "maladie"},
-  {nom : "Paule" , date : '14/05/26' , motif : "maladie"},
-  {nom : "Jerie" , date : '14/05/26' , motif : "maladie"},
-  {nom : "Eric" , date : '14/05/26' , motif : "maladie"},
+  { nom: "Bryan", date: '14/05/26', motif: "maladie" },
+  { nom: "Jean", date: '14/05/26', motif: "maladie" },
+  { nom: "Paule", date: '14/05/26', motif: "maladie" },
+  { nom: "Jerie", date: '14/05/26', motif: "maladie" },
+  { nom: "Eric", date: '14/05/26', motif: "maladie" },
 ]
 const page = () => {
   return (
     <div className="min-h-screen p-5 space-y-4">
       <div className="grid grid-cols-4 gap-2 ">
-          {statItems.map((item) =>(
-            <Card key={item.titre} className="shadow-none rounded-sm hover:scale-y-105 hover:shadow-sm transition-all duration-300 hover:bg-sky-50">
-              <CardHeader>
-                <h3 className="font-semibold ">{item.titre}</h3>
-              </CardHeader>
-              <CardContent className="flex justify-between items-end">
-                <h1 className="font-bold text-4xl">{item.nombre}</h1>
-                <p className="font-semibold">{item.pourcent}</p>
-              </CardContent>
-            </Card>
-            
-          ) )}
-      </div>
-      
-        <main className="max-h-2xl h-full  flex w-full gap-3">
-          <Card className="w-full   rounded-sm  shadow-none p-5 max-h-96 ">
-            <CardHeader className=" flex items-center gap-3">
-              
-              <Hourglass />
-              <p className="font-semibold">Demande Obtenue</p>
+        {statItems.map((item) => (
+          <Card key={item.titre} className="shadow-none rounded-sm hover:scale-y-105 hover:shadow-sm transition-all duration-300 hover:bg-sky-50 dark:hover:bg-slate-700">
+            <CardHeader>
+              <h3 className="font-semibold ">{item.titre}</h3>
             </CardHeader>
-              <CardContent className=" flex flex-col gap-2 overflow-auto ">
-                {demandeItem.map((item) => (
-                 <div
+            <CardContent className="flex justify-between items-end">
+              <h1 className="font-bold text-4xl">{item.nombre}</h1>
+              <p className="font-semibold">{item.pourcent}</p>
+            </CardContent>
+          </Card>
+
+        ))}
+      </div>
+
+      <main className="max-h-2xl h-full  flex w-full gap-3">
+        <Card className="w-full   rounded-sm  shadow-none p-5 max-h-96 ">
+          <CardHeader className=" flex items-center gap-3">
+
+            <Hourglass />
+            <p className="font-semibold">Demande Obtenue</p>
+          </CardHeader>
+          <CardContent className=" flex flex-col gap-2 overflow-auto ">
+            {demandeItem.map((item) => (
+              <div
                 key={item.titre}
                 className="group border border-dashed flex  justify-between  px-4 py-3 rounded-sm hover:bg-slate-50 dark:hover:bg-zinc-700 dark:hover:scale-y-105 transition-all duration-300 cursor-default"
               >
@@ -73,22 +77,22 @@ const page = () => {
                   </div>
                 </div>
                 <div className="flex justify-between items-center gap-2">
-                  <Button  variant='outline' className="bg-green-50 text-green-400 border-green-400 hover:bg-green-500"><Check /></Button>
+                  <Button variant='outline' className="bg-green-50 text-green-400 border-green-400 hover:bg-green-500"><Check /></Button>
                   <Button variant="outline" className="bg-red-50 border-red-400 text-red-400 hover:bg-red-500 hover:text-white"><X /></Button>
                 </div>
-                
+
               </div>
-              ))}
-              </CardContent>
-          </Card>
-          <Card className="container w-2/3  h-full max-h-96 shadow-none">
-            <CardHeader>
-              <span className="flex items-center gap-3"><ClipboardClock />
+            ))}
+          </CardContent>
+        </Card>
+        <Card className="container w-2/3  h-full max-h-96 shadow-none rounded-sm">
+          <CardHeader>
+            <span className="flex items-center gap-3"><ClipboardClock />
               <p className="font-semibold">Absence ou retard </p></span>
-            </CardHeader>
-            <CardContent className="flex flex-col gap-2 overflow-auto ">
-               {billetItem.map((item) => (
-                 <div
+          </CardHeader>
+          <CardContent className="flex flex-col gap-2 overflow-auto ">
+            {billetItem.map((item) => (
+              <div
                 key={item.nom}
                 className="group border border-dashed flex  justify-between  px-4 py-3 rounded-sm hover:bg-slate-50 dark:hover:bg-zinc-700 dark:hover:scale-y-105 transition-all duration-300 cursor-default"
               >
@@ -104,48 +108,48 @@ const page = () => {
                   </div>
                 </div>
                 <div className="flex justify-between items-center gap-2">
-                  <Button  variant='outline' className="bg-green-50 text-green-400 border-green-400 hover:bg-green-500"><Check /></Button>
+                  <Button variant='outline' className="bg-green-50 text-green-400 border-green-400 hover:bg-green-500"><Check /></Button>
                   <Button variant="outline" className="bg-red-50 border-red-400 text-red-400 hover:bg-red-500 hover:text-white"><X /></Button>
                 </div>
-                
-              </div>
-              ))}
 
-            </CardContent>
-          </Card>
-        </main>
-        <Card className="border h-2/3 max-h-72 rounded-sm">
-          <CardHeader>
-            <span className="flex items-start gap-2 font-semibold"><CalendarDays />Demande des Reunion</span>
-             
-          </CardHeader>
-          <CardContent className="flex flex-col gap-2 overflow-auto ">
-               {billetItem.map((item) => (
-                 <div
-                key={item.nom}
-                className="group border border-dashed flex  justify-between  px-4 py-3 rounded-sm hover:bg-slate-50 dark:hover:bg-zinc-700 dark:hover:scale-y-105 transition-all duration-300 cursor-default"
-              >
-                <div className="flex items-center">
-                  <div>
-                    <span className="flex gap-2 items-center">{item.nom} le {item.date}</span>
-                    <p className="text-sm font-medium text-slate-700 dark:text-slate-400 leading-tight">
-                      {item.motif}
-                    </p>
-                   
-                  </div>
-                </div>
-                <div className="flex justify-between items-center gap-2">
-                  <Button  variant='outline' className="bg-green-50 text-green-400 border-green-400 hover:bg-green-500"><Check /></Button>
-                  <Button variant="outline" className="bg-red-50 border-red-400 text-red-400 hover:bg-red-500 hover:text-white"><X /></Button>
-                </div>
-                
               </div>
-              ))}
+            ))}
 
-            </CardContent>
-          
+          </CardContent>
         </Card>
-      
+      </main>
+      <Card className="border h-2/3 max-h-72 rounded">
+        <CardHeader>
+          <span className="flex items-start gap-2 font-semibold"><CalendarDays />Demande des Reunion</span>
+
+        </CardHeader>
+        <CardContent className="flex flex-col gap-2 overflow-auto  rounded-sm">
+          {billetItem.map((item) => (
+            <div
+              key={item.nom}
+              className="group border border-dashed flex  justify-between  px-4 py-3 rounded-sm hover:bg-slate-50 dark:hover:bg-zinc-700 dark:hover:scale-y-105 transition-all duration-300 cursor-default"
+            >
+              <div className="flex items-center">
+                <div>
+                  <span className="flex gap-2 items-center">{item.nom} le {item.date}</span>
+                  <p className="text-sm font-medium text-slate-700 dark:text-slate-400 leading-tight">
+                    {item.motif}
+                  </p>
+
+                </div>
+              </div>
+              <div className="flex justify-between items-center gap-2">
+                <Button variant='outline' className="bg-green-50 text-green-400 border-green-400 hover:bg-green-500"><Check /></Button>
+                <Button variant="outline" className="bg-red-50 border-red-400 text-red-400 hover:bg-red-500 hover:text-white"><X /></Button>
+              </div>
+
+            </div>
+          ))}
+
+        </CardContent>
+
+      </Card>
+
     </div>
   )
 }
